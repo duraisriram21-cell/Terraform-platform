ca645f666c104ca74b18894dba8c0fbdd51b922c
sramadu3 <Srinidhi.Ramadurai3@T-Mobile.com>
2025-11-21 11:28:05 -0800

    Added production environment (prod) structure and s3 resources

---
+++ envs/prod/outputs.tf
@@ -0,0 +1,4 @@
+output "demo_bucket_name" {
+  description = "Name of the S3 bucket created in dev"
+  value       = aws_s3_bucket.demo_bucket.bucket
+}
---
+++ envs/prod/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for this environment"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "project_name" {
+  description = "Name of the project for naming resources"
+  type        = string
+  default     = "terraform-platform"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
